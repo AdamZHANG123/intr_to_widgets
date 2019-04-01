de860b6c19153552d24aa787dae7f9f6490203e4
Adam Zhang <adamisagoodguy@gmail.com>
2019-04-01 12:16:40 +0800

    Initial commit

---
+++ android/app/src/main/java/hk/com/netizen/test/intr_to_widgets/MainActivity.java
@@ -0,0 +1,13 @@
+package hk.com.netizen.test.intr_to_widgets;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
