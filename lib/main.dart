cd7551c955ca51ee2835aa311a6d4a183aa81b4c
Adam Zhang <adamisagoodguy@gmail.com>
2019-04-03 11:22:58 +0800

    add interactive to the vacation page
    change the favorite button and text from a row of two widget, to a stateful class which has a state that builds a column of the two widgets.
    
    The state instance can store informations that might change when user taps or do other thing that interacts with the app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 // for different layout modules, import different dart files below.
 // import 'card.dart';
 // import 'dropdown_btn.dart';
-//import 'vacation.dart';
+import 'vacation.dart';
 //import 'transform_example.dart';
-import 'opacity_test.dart';
+//import 'opacity_test.dart';
 
 void main() => runApp(MyApp());
 
