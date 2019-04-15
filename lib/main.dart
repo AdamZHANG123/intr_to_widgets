a2b7159951a0c0eb37f72ce8f2b82741003a9ba7
Adam Zhang <adamisagoodguy@gmail.com>
2019-04-15 15:58:54 +0800

    add animated_opacity module
    Added AnimatedOpacity Demo

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@
 
 //import 'hero_animation.dart';
 //import 'http_get.dart';
-import 'animated_container.dart';
+//import 'animated_container.dart';
+import 'animated_opacity.dart';
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
