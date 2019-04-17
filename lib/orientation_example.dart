bd0bef1918ce765916711241fc010fb2c60338f5
Adam Zhang <adamisagoodguy@gmail.com>
2019-04-17 21:59:56 +0800

    Update Orientation_example module
    Updated orientation_example module, Defined ThemeData and Used ThemeData.

--- lib/orientation_example.dart
+++ lib/orientation_example.dart
@@ -4,6 +4,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+          // Define the Brightness and Colors
+          brightness: Brightness.dark,
+          primaryColor: Colors.lightBlue[800],
+          accentColor: Colors.cyan[600],
+
+          // Define the default Font Family
+          fontFamily: 'Montserrat',
+          textTheme: TextTheme(
+            headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+            title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+            body1: TextStyle(fontSize: 14.0, fontFamily: 'Hid'),
+          )),
       home: HomePage(),
     );
   }
@@ -16,17 +29,36 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Orientation Example'),
       ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-              crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-              children: List.generate(
-                100,
-                (index) => ListTile(
-                      title: Text('item $index'),
-                    ),
-              ));
-        },
+      body: Column(
+        children: <Widget>[
+          Text(
+            'Text with a background color',
+            style: Theme.of(context).textTheme.title,
+          ),
+          Flexible(
+            child: OrientationBuilder(
+              builder: (context, orientation) {
+                return GridView.count(
+                    crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
+                    children: List.generate(
+                      100,
+                      (index) => ListTile(
+                            title: Text('item $index'),
+                          ),
+                    ));
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          accentColor: Colors.yellow,
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
